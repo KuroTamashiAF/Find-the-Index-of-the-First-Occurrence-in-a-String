c1ba43dbe13d3f80639e0b1c8de73b12d488cd23
Fedorov <chugun777akk@mail.ru>
2023-03-21 09:29:28 +0700

    add main

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,21 @@
 #include<iostream>
 using namespace std;
 
+class Solution {
+public:
+    static int strStr(string haystack, string needle) {
+
+    }
+};
+
 
 
 
 int main()
 {
+    string input = "sadbutsad";
+    string aim = "sad";
+    cout << Solution::strStr(input, aim);
 
+    return 0;
 }
