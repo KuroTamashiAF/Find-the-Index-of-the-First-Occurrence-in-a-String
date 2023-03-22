3202a63e32a79962f460d7a05c43eeb642222a62
Fedorov <chugun777akk@mail.ru>
2023-03-22 07:54:46 +0700

    create source

--- Source.cpp
+++ Source.cpp
@@ -1,10 +1,27 @@
 #include<iostream>
 using namespace std;
 
+//class Solution {
+//public:
+//    static int strStr(string haystack, string needle) {
+//        return haystack.find(needle);
+//    }
+//};
 class Solution {
 public:
     static int strStr(string haystack, string needle) {
+        int pointer = 0;
 
+        for (int i = 0; i < haystack.size(); i++)
+        {
+            if (haystack[i] == needle[pointer])
+            {
+                for (int i = 0; i < haystack.size(); i++)
+                {
+
+                }
+            }
+        }
     }
 };
 
@@ -14,7 +31,7 @@ public:
 int main()
 {
     string input = "sadbutsad";
-    string aim = "sad";
+    string aim = "but";
     cout << Solution::strStr(input, aim);
 
     return 0;
